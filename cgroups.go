f7a2bf91103ae8d59fbd0d682fa68443db30c668
felipetojal <felipetojal07@gmail.com>
2026-06-10 17:07:23 -0300

    Added functions to modify the pids.max and memory.max virtual files

--- cgroups.go
+++ cgroups.go
@@ -6,27 +6,36 @@ import (
 	"strconv"
 )
 
-func setupCgroups(pid int, name string) error {
-	if err := setupCgroupDir(name); err != nil {
+func setupCgroups(pid int, contName string, memMax int, pidMax int) error {
+	err, dirName := setupCgroupDir(contName)
+	if err != nil {
 		return fmt.Errorf("Error in setupCgroups(): %w", err)
 	}
 
-	if err := setupPidCgroup(pid, name); err != nil {
+	if err := setupPidCgroup(pid, dirName); err != nil {
 		return fmt.Errorf("Error in setupCgroups(): %w", err)
 	}
 
+	if err := setupMemoryMax(memMax, dirName); err != nil {
+		return fmt.Errorf("Error in setupCgroups(): %w", err)
+	}
+
+	if err := setupMaxPid(pidMax, dirName); err != nil {
+		return fmt.Errorf("Error in setupCgroups(): %w", err)
+	}
+	
 	return nil
 }
 
 // setupCgroupDir creates the cgroup directory.
 // Once the dir is created, the Linux Kernel automatically populates it
 // with virtual text files.
-func setupCgroupDir(name string) error {
+func setupCgroupDir(name string) (error, string) {
 	dirName := fmt.Sprintf("/sys/fs/cgroups/%s", name)
 	if err := os.Mkdir(dirName, 0755); err != nil {
-		return fmt.Errorf("Error creating dir: %w", err)
+		return fmt.Errorf("Error creating dir: %w", err), ""
 	}
-	return nil
+	return nil, dirName
 }
 
 // setupPidCgroup writes the process PID associated to the folder.
@@ -35,6 +44,28 @@ func setupPidCgroup(pid int, dirPath string) error {
 	data := []byte(strconv.Itoa(pid))
 	if err := os.WriteFile(filePath, data, 0755); err != nil {
 		return fmt.Errorf("Error writing cgroups.procs: %w", err)
+	}
+	return nil
+}
+
+// setupMemoryMax writes to the memory.max virtual file. It is responsible
+// for specifying how much RAM the process is allowed to consume.
+func setupMemoryMax(max int, dirPath string) error {
+	filePath := fmt.Sprintf("%s/memory.max", dirPath)
+	data := []byte(strconv.Itoa(max))
+	if err := os.WriteFile(filePath, data, 0755); err != nil {
+		return fmt.Errorf("Error writing memory.max: %w", err)
+	}
+	return nil
+}
+
+// setupMaxPid writes to the pids.max virtual file. It is responsible
+// spcecifying the maximum number of process the container can hold.
+func setupMaxPid(max int, dirName string) error {
+	filePath := fmt.Sprintf("%s/pids.max", dirName)
+	data := []byte(strconv.Itoa(max))
+	if err := os.WriteFile(filePath, data, 0755); err != nil {
+		return fmt.Errorf("Error writing max PID: %w", err)
 	}
 	return nil
 }
